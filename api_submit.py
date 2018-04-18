3a158688e509c3ab59c7bcae87f1e21a6ca6f5bc
Po Chun, Lu <oceanus11034@gmail.com>
2018-04-19 00:27:34 +0800

    [Add] london sample average file

--- api_submit.py
+++ api_submit.py
@@ -3,13 +3,13 @@
 import os
 import requests
 
-files = {'files': open('sample_submission.csv', 'rb')}
+files = {'files': open('submission.csv', 'rb')}
 
 data = {
     # user_id is your username which can be found on the top-right corner on our website when you logged in.
     "user_id": os.environ['KDD_CUP_USER_ID'],
     "team_token": os.environ['KDD_CUP_TOKEN'],  # your team_token.
-    "description": '2017 average',  # no more than 40 chars.
+    "description": '2017 average sample',  # no more than 40 chars.
     "filename": "submission.csv",  # your filename
 }
 
--- main.py
+++ main.py
@@ -7,25 +7,38 @@ def getMeanValue(data, columnName):
   return data[['stationId', columnName]].groupby(['stationId'], as_index=False).mean()
 
 
-def getData(dataPath):
+def getData(dataPath, isBeijing=False):
   train_df = pd.read_csv(dataPath)
   pm_two = getMeanValue(train_df, 'PM2.5')
   pm_ten = getMeanValue(train_df, 'PM10')
-  o_three = getMeanValue(train_df, 'O3')
-  return [pm_two, pm_ten, o_three]
+  data = [pm_two, pm_ten]
+
+  if (isBeijing):
+    o_three = getMeanValue(train_df, 'O3')
+    data.append(o_three)
+
+  return data
 
 def writeFile(output, data):
   for id, station in enumerate(data[0]['stationId']):
-    for hour in range(1):
+    for hour in range(48):
       output.write(data[0]['stationId'][id])
+      output.write('#' + str(hour))
+      output.write(',')
       output.write(str(data[0]['PM2.5'][id]))
-      output.write(str(data[1]['PM10'][id]))
-      output.write(str(data[2]['O3'][id]))
+      if (str(data[1]['PM10'][id]) != 'nan'):
+        output.write(',')
+        output.write(str(data[1]['PM10'][id]))
+      if(len(data) > 2):
+        output.write(',')
+        output.write(str(data[2]['O3'][id]))
       output.write('\n')
 
-dataBeijing = getData('./data/beijing/beijing_17_18_aq.csv')
+dataBeijing = getData('./data/beijing/beijing_17_18_aq.csv', isBeijing=True)
+dataLondon_1 = getData('./data/london/London_historical_aqi_forecast_stations_20180331.csv')
 
 with open('submission.csv', 'w') as output:
   output.write('test_id,PM2.5,PM10,O3\n')
   writeFile(output, dataBeijing)
+  writeFile(output, dataLondon_1)
   
